36e5f230481f93f5d2a3d68dd243289f662ea463
Joshua Plicque <plicjo@gmail.com>
2019-03-17 23:38:11 -0400

    Redo Binary Gap

--- binary_gap.rb
+++ binary_gap.rb
@@ -1,8 +1,5 @@
 def binary_gap(n)
-  gaps = n.to_s(2).scan(/1?0+1/).map do |string|
-    string.gsub('1', '')
-  end
-
-  return 0 if gaps.empty?
-  gaps.max.length
+  results = n.to_s(2).scan(/(?<=1)0+(?=1)/)
+  return 0 if results.empty?
+  results.max.length
 end
